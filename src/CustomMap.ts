e430335ba93d61a3647f1e2b7cec5b47b0d5247c
sefatanam <sefatanam@gmail.com>
2021-03-23 22:45:41 +0600

    showing content

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -41,12 +41,22 @@ export class CustomMap {
 
 
     addMarker(mappable: Mappable) {
-        new google.maps.Marker({
+
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
                 lng: mappable.location.lng
             }
+        })
+
+
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: "Hi There"
+            })
+
+            infoWindow.open(this.googleMap, marker);
         })
     }
 }
