0c57ed787ab26086ef0586d8ab7027be1bda2630
sefatanam <sefatanam@gmail.com>
2021-03-23 23:50:50 +0600

    number collection added

--- sortalgo/src/Sorter.ts
+++ sortalgo/src/Sorter.ts
@@ -2,19 +2,13 @@
 import { NumbersCollection } from './NumbersCollection';
 
 export class Sorter {
-    constructor(public collection: any[]) { }
-
+    constructor(public collection: NumbersCollection) { }
     sort(): void {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - 1 - i; j++) {
-
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
+                if (this.collection.compare(j, j + 1)) {
+                    this.collection.swap(j, j + 1);
                 }
             }
         }
--- sortalgo/src/index.ts
+++ sortalgo/src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hello There 12 4');
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
