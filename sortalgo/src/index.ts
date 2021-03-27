71020e626f3df08b3341ccd5f27b86becbb75ea6
sefatanam <sefatanam@gmail.com>
2021-03-27 15:32:54 +0600

    linked list implemented

--- sortalgo/src/index.ts
+++ sortalgo/src/index.ts
@@ -1,12 +1,24 @@
 import { Sorter } from './Sorter';
-import { NumbersCollection } from './NumbersCollection';
-import { CharactersCollection } from './CharactersCollection';
+// import { NumbersCollection } from './NumbersCollection';
+// import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([10, 3, 100, 1000, -5, 1, 2, 4, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([3, 2, 1]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+// const charactersCollection = new CharactersCollection('adbca');
+// const sorter2 = new Sorter(charactersCollection);
+// sorter2.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(3);
+linkedList.add(2);
+linkedList.add(1);
+linkedList.add(0);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(numbersCollection.data);
-const charactersCollection = new CharactersCollection('adbca');
-const sorter2 = new Sorter(charactersCollection);
-sorter2.sort();
-console.log(charactersCollection.data);
+
+linkedList.print();
