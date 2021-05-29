9d940769448e64af6d611141da1b999f5ee4c3b7
sefatanam <sefatanam@gmail.com>
2021-05-30 01:53:04 +0600

    feat : quokkas.js is awesome

--- funcTS/src/index.ts
+++ funcTS/src/index.ts
@@ -1,5 +1,13 @@
-import { objectName } from "./functionUsingObject";
 
-objectName({ name: 'Sefat' });
-objectName({ address: 'Dhaka' });
 
+const strings: string[] = ['🙆‍♂️', '😎', '💡'];
+console.log(typeof strings[0])  // Output =>
+
+const numbers = [1, 2, 3, 4];
+console.log(typeof numbers[3]);  // Output =>
+
+console.log(Name)
+
+function Name() {
+  return 4;
+}
