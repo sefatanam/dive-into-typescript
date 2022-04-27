fa16481aa301ca96b8b589e725adff7e0dc52775
sefatanam <sefatanam@gmail.com>
2022-04-27 23:40:03 +0600

    decorator

---
+++ advance-ts/src/decorators/class.ts
@@ -0,0 +1,30 @@
+
+// // Example with class extends
+// class BankCard {
+//     constructor(public cardId: number) {
+//         this.cardId = 10000022;
+//     }
+// }
+
+// class IFICBank extends BankCard {
+
+//     constructor(public name: string) {
+//         super(10002);
+//         this.name = name;
+//     }
+// }
+
+// Example with decorators
+
+function DBankCard(constructor: Function) {
+    constructor.prototype.cardId = Math.floor(Math.random() * 100);
+}
+
+@DBankCard
+class EBLBank {
+    constructor(public name: string) {
+        this.name = name;
+    }
+}
+
+let result = new EBLBank("Sefat");
