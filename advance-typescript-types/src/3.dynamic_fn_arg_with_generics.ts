45feaff6167cb683c8866e098ace5c7b37a1a4af
sefatanam <sefatanam@gmail.com>
2025-03-25 03:32:20 +0600

    chore(att): move dead code

--- advance-typescript-types/src/3.dynamic_fn_arg_with_generics.ts
+++ advance-typescript-types/src/3.dynamic_fn_arg_with_generics.ts
@@ -1,11 +1,13 @@
-type EventType = {
-    type: "LOGIN",
-    payload: {
-        userId: 1
-    }
-} | {
-    type: 'SIGN_OUT'
-}
+type EventType =
+	| {
+			type: "LOGIN";
+			payload: {
+				userId: 1;
+			};
+	  }
+	| {
+			type: "SIGN_OUT";
+	  };
 
 /* BAD */
 /* const sendEvent = (eventType: EventType['type'], payload?: any) => { }
@@ -15,10 +17,13 @@ sendEvent('SIGN_OUT', {}) // not type safe
 sendEvent('SIGN_OUT', { userId: 1 }) // not type safe */
 
 /* GOOD */
-const sendEvent = <TType extends EventType['type']>
-    (...args: Extract<EventType, { type: TType }> extends { payload: infer TPayLoad }
-        ? [type: TType, payload: TPayLoad]
-        : [type: TType]) => { }
+// TODO: Implement sendEvent function
+//
+const sendEvent = <T extends EventType["type"]>(
+	...args: Extract<EventType, { type: T }> extends { payload: infer TPayLoad }
+		? [type: T, payload: TPayLoad]
+		: [type: T]
+) => {};
 
-sendEvent('LOGIN', { userId: 1 })
-sendEvent('SIGN_OUT')
+sendEvent("LOGIN", { userId: 1 });
+sendEvent("SIGN_OUT");
