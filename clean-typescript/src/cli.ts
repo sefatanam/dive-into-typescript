84f156fd9b8ab86e1c964cca54ebbb8e30168089
sefatanam <sefatanam@gmail.com>
2025-05-18 00:48:36 +0600

    refactor: restructure CLI command handling and tests

--- clean-typescript/src/cli.ts
+++
@@ -1,6 +0,0 @@
-import { CommandFactory } from './command-factory.js';
-
-export function runCli(args: string[]): string {
-  const command = CommandFactory.createCommand(args);
-  return command.execute();
-}
