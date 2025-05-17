11547cb6dc62ded6c587730b716c43e29fc42999
Sefat Anam <37630292+sefatanam@users.noreply.github.com>
2025-05-18 00:53:38 +0600

    Vitest (#8)
    * feat: implement CLI functionality with greeting and help commands
    
    - Added `runCli` function in `cli.ts` to handle command line arguments.
    - Updated `index.ts` to read arguments and output results from `runCli`.
    - Created tests for `runCli` in `cli.test.ts` to ensure correct behavior for greeting, help, and argument logging.
    - Modified `tsconfig.json` to adjust `rootDir` for better module resolution.
    - Added Vitest configuration for testing setup.
    
    * feat: implement command pattern for CLI with greet and help commands
    
    * refactor: restructure CLI command handling and tests

--- clean-typescript/src/index.ts
+++ clean-typescript/src/index.ts
@@ -1,22 +1,5 @@
 import process from 'node:process';
 import { runCli } from './cmd/cli.js';
 
-const cliRs = runCli(process.argv.slice(2));
-console.log(cliRs);
-
-export type Color = 'red' | 'green' | 'blue' | 'yellow' | 'magenta' | 'cyan' | 'white' | 'black' | (string & {});
-export type ColorCode = 31 | 32 | 33 | 34 | 35 | 36 | 37 | 90;
-export type ColorCodeBg = 41 | 42 | 43 | 44 | 45 | 46 | 47 | 100;
-export type ColorCodeBright = 91 | 92 | 93 | 94 | 95 | 96 | 97;
-
-
-export function colorize(text: string, color: Color | ColorCode): string {
-  return `\x1b[${color}m${text}\x1b[0m`;
-}
-
-
-const result = colorize('Hello World', 'black');
-
-
-
+const result = runCli(process.argv.slice(2));
 console.log(result);
